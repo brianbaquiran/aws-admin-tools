80f0635b928616880f1271c2dd988fb7c6beb44a
Brian Baquiran <brianbaquiran@gmail.com>
2016-05-20 20:57:23 +0800

    Download CloudWatch scripts to /tmp and install under /usr/local/bin.

--- user-data/messaging-server.sh
+++ user-data/messaging-server.sh
@@ -5,12 +5,16 @@ yum update -y
 # Documentation: http://docs.aws.amazon.com/AmazonCloudWatch/latest/DeveloperGuide/mon-scripts.html
 # Download: http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip
 yum install -y perl-Switch perl-DateTime perl-Sys-Syslog perl-LWP-Protocol-https
-su -l ec2-user "wget http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip"
-su -l ec2-user "unzip CloudWatchMonitoringScripts-1.2.1.zip"
-su -l ec2-user "rm CloudWatchMonitoringScripts-1.2.1.zip"
-cat > /home/ec2-user/aws-scripts-mon/crontab << EOF
-*/5 * * * * /home/ec2-user/aws-scripts-mon/mon-get-instance-stats.pl --from-cron --mem-util --mem-used --mem-avail --swap-util --swap-used --disk-path=/ --disk-space-util --disk-space-used --disk-space-avail
+cd /tmp
+wget http://aws-cloudwatch.s3.amazonaws.com/downloads/CloudWatchMonitoringScripts-1.2.1.zip
+cd /usr/local/bin/
+unzip /tmp/CloudWatchMonitoringScripts-1.2.1.zip
+
+# Create a crontab file for ec2-user
+cat > /tmp/crontab << EOF
+*/5 * * * * /usr/local/bin/aws-scripts-mon/mon-put-instance-data.pl --from-cron --mem-util --mem-used --mem-avail --swap-util --swap-used --disk-path=/ --disk-space-util --disk-space-used --disk-space-avail
 EOF
+crontab -u ec2-user /tmp/crontab
 
 # Install Perlbrew http://perlbrew.pl/
 # Amazon Linux has perlbrew in the yum repository already
